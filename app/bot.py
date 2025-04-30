03a6edd0e1886dab31e3cfce347342d0f8163c7e
Thalys M <thalyscorreia@hotmail.com>
2025-04-30 17:06:50 -0300

    feat: inicialização do bot e instanciamento dos comandos iniciais

---
+++ app/bot.py
@@ -0,0 +1,18 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+from app.commands import show_schedule, show_news, show_players, cheer, start, collections, ask
+from config import TELEGRAM_TOKEN
+
+def run_bot():
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    app.add_handler(CommandHandler("schedule", show_schedule.handle))
+    app.add_handler(CommandHandler("news", show_news.handle))
+    app.add_handler(CommandHandler("players", show_players.handle))
+    app.add_handler(CallbackQueryHandler(show_players.button))
+    app.add_handler(CommandHandler("cheer", cheer.handle))
+    app.add_handler(CommandHandler("start", start.handle))
+    app.add_handler(CommandHandler("collections", collections.handle))
+    app.add_handler(CommandHandler("ask", ask.handle))
+
+    print("Bot está rodando...")
+    app.run_polling()
